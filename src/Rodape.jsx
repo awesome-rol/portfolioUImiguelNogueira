fc201021af69d1850cb1941f9c3c39b9499419c7
Miguel <miguel.gomes1206@gmail.com>
2025-07-10 04:15:45 -0300

    troca whatsapp

--- src/Rodape.jsx
+++ src/Rodape.jsx
@@ -14,7 +14,7 @@ const Rodape = ({instagram, linkedin, whatsapp, alternativoLogo}) => {
       </div>
       <div className="rodapeBotoes">
         <SkillButton link="https://www.linkedin.com/in/miguel-nogueira-a7b9a922a/" icone={linkedinImg} style={{width: "80px"}}/>
-        <SkillButton link="https://wa.me/5582987654321?text=Ol%C3%A1%2C%20vi%20seu%20portf%C3%B3lio%20e%20gostaria%20de%20falar%20com%20voc%C3%AA." icone={whats} />
+        <SkillButton link="https://wa.me/5582988136875?text=Ol%C3%A1%2C%20vi%20seu%20portf%C3%B3lio%20e%20gostaria%20de%20falar%20com%20voc%C3%AA." icone={whats} />
         <SkillButton link="https://github.com/awesome-rol" icone={github} />
       </div>
     </div>
