bf368f5677dfabb52f097408b6e30987e30152ca
Miguel <miguel.gomes1206@gmail.com>
2025-07-10 04:12:16 -0300

    troca insta por github

--- src/SkillButton.jsx
+++ src/SkillButton.jsx
@@ -3,7 +3,7 @@ import './SkillButton.css'
 
 const SkillButton = ({icone, alternativo, link}) => {
   return (
-    <a className="skillButtonLink" href={link}>
+    <a className="skillButtonLink" href={link} target='_blank'>
         <div className='skillButtonContainer'>
             <div className="skillButton-icone"><img className="skillButton-iconeImagem" src={icone} alt={alternativo} /></div>
             <div className="skillButton-sombra"></div>
