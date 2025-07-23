329cc80483f9d04bcf4c2d5456721ff666d15065
Miguel <miguel.gomes1206@gmail.com>
2025-07-23 17:48:53 -0300

    adição icone behance rodapé

--- src/Rodape.jsx
+++ src/Rodape.jsx
@@ -5,6 +5,7 @@ import logoRodape from './assets/imagens/logo.png';
 import linkedinImg from './assets/imagens/brancos/branco.png';
 import whats from './assets/imagens/brancos/whats.png';
 import github from './assets/imagens/brancos/github.png';
+import behance from './assets/imagens/brancos/behance.png';
 
 const Rodape = ({instagram, linkedin, whatsapp, alternativoLogo}) => {
   return (
@@ -16,6 +17,7 @@ const Rodape = ({instagram, linkedin, whatsapp, alternativoLogo}) => {
         <SkillButton link="https://www.linkedin.com/in/miguel-nogueira-a7b9a922a/" icone={linkedinImg} style={{width: "80px"}}/>
         <SkillButton link="https://wa.me/5582988136875?text=Ol%C3%A1%2C%20vi%20seu%20portf%C3%B3lio%20e%20gostaria%20de%20falar%20com%20voc%C3%AA." icone={whats} />
         <SkillButton link="https://github.com/awesome-rol" icone={github} />
+        <SkillButton link="https://www.behance.net/miguelnogueira14" icone={behance} />
       </div>
     </div>
   );
